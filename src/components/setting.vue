ee8890d74d82cedeaa48dc2c16ccf3ce7c674b99
CodFrm <code.farmer@qq.com>
2018-08-07 11:46:44 +0800

    设置页面

--- src/components/setting.vue
+++ src/components/setting.vue
@@ -8,13 +8,13 @@
                 <div class="card-text">
                     <div class="box-content form">
                         <div class="ipt-group">
-                           <label class="tag">同时在线:</label><input class="ipt-text" type="number" placeholder="允许同时在线的个数" name="online_num" v-model.number="online_num">
+                           <label class="tag">同时在线:</label><input class="ipt-text" type="number" placeholder="允许同时在线的个数" name="online_num" v-model.number="config.online_num">
                         </div>
                         <div class="ipt-group">
-                           <label class="tag">测试巴打实:</label><input class="ipt-text" type="text" placeholder="允许同时在线的个数">
+                           <label class="tag">注册间隔:</label><input class="ipt-text" type="number" placeholder="注册间隔长度(秒)" name="reg_interval" v-model.number="config.reg_interval">
                         </div>
                         <div class="ipt-group" style="text-align: right;margin-top:20px;">
-                            <button class="btn" style="background: #f23b3b;color: #fff;">修改</button>
+                            <button class="btn" style="background:#f23b3b;color:#fff;height:30px;width:80px;font-size:14px;">修改</button>
                         </div>
                     </div>
                 </div>
@@ -32,8 +32,26 @@ export default {
   name: "setting",
   data() {
     return {
-      online_num: 1
+      config: {
+        online_num: 1,
+        reg_interval: 3600
+      }
     };
+  },
+  created() {
+    this.getConfig();
+  },
+  methods: {
+    getConfig() {
+      var vue = this;
+      get(config.url + config.aapi + "setting")
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(json) {
+          vue.config = json["config"];
+        });
+    }
   }
 };
 </script>
